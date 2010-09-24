6471045d0d16a9bd0c5877a7969cd4b74999e042
Kirill Panshin <kipanshi@gmail.com>
2010-09-24 07:32:12 +0400

    menu implementation in progress

--- menu/urls.py
+++ menu/urls.py
@@ -2,13 +2,17 @@ from django.conf.urls.defaults import *
 from pt.menu.models import News, Info 
 from django.contrib.auth.models import User
 
-info_dict = {
+news_dict = {
     'queryset': News.objects.all(),
+}
+info_dict = {
+	'queryset': Info.objects.all(),
 }
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', dict(info_dict, template_name='main.html')),
-    (r'^/content/(?P<object_id>\d+)/', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='detail.html')),
-    (r'^/news/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='detail.html')),
+    url(r'^$', 'django.views.generic.list_detail.object_list', dict(news_dict, template_name='main.html')),
+    url(r'^cont/(?P<info_id>\d+)/list/$', 'list'),
+    url(r'^cont/(\d+)/list/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='detail_cont.html')),
+    url(r'^news/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', dict(news_dict, template_name='detail.html')),
 )
 
--- menu/views.py
+++ menu/views.py
@@ -1,2 +1,15 @@
 # Create your views here.
 
+from django.views.generic.list_detail import object_list
+
+from models import Info
+
+#def info_list(request, object_id, **kwargs):
+#	queryset=Info.objects.filter(pk=object_id)
+#   	return object_list(request, queryset=queryset, template_object_name='main_cont.html', **kwargs)
+    
+def list(request, info_id):
+    queryset=Info.objects.filter(pk=info_id)
+    return object_list(request, dict(queryset, template_name='main_cont.html'))
+
+
