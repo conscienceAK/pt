2837733dc1c99ff689b68e056485b3c6fee62d3a
Kirill Panshin <kipanshi@gmail.com>
2010-09-24 05:25:32 +0400

    implementing news

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from models import Menu, MenuItem, Info
+from models import * 
 
 admin.site.register(Menu)
 admin.site.register(MenuItem)
 admin.site.register(Info)
+admin.site.register(News)
+#admin.site.register(Topblogs)
--- menu/urls.py
+++ menu/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls.defaults import *
-from pt.menu.models import Menu, Info 
+from pt.menu.models import News, Info 
 from django.contrib.auth.models import User
 
 info_dict = {
-    'queryset': Menu.objects.all(),
+    'queryset': News.objects.all(),
 }
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', dict(info_dict, template_name='base.html')),
-    (r'^/content/(?P<object_id>\d+)/', 'django.views.generic.list_detail.object_detail', info_dict),
+    (r'^$', 'django.views.generic.list_detail.object_list', dict(info_dict, template_name='main.html')),
+    (r'^/content/(?P<object_id>\d+)/', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='detail.html')),
+    (r'^/news/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', dict(info_dict, template_name='detail.html')),
 )
 
